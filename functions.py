5275fed4ec9c99fe8283c96c0a380f49ed731f83
mbmerodata <66883727+mbmerodata@users.noreply.github.com>
2021-11-14 11:41:39 -0300

    se agregaron cambios

--- functions.py
+++ functions.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 from datetime import datetime, timedelta
+from varname import varname, nameof
+
 
 #TF
 from keras.models import Sequential
